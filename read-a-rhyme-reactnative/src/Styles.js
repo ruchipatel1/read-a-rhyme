547fc17be64825c2c737917dbf31246f908410f8
Randy Li <randyli123@outlook.com>
2021-09-20 12:32:10 -0400

    updates on UI of reading page

--- read-a-rhyme-reactnative/src/Styles.js
+++ read-a-rhyme-reactnative/src/Styles.js
@@ -7,5 +7,26 @@ export const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         padding: 20
+    },    
+    reading: {
+        height: 300,
+        width: 200,
+        borderRadius: 5,
+        marginVertical: 40,
+        backgroundColor: "#cccccc",
+        alignItems: "center",
+        justifyContent: "center",
+        marginRight: 20
+
     },
+    readingText: {
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#cccccc",
+
+    },
+    row: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+      },
 });
--- read-a-rhyme-reactnative/src/screens/ReadingScreen.js
+++ read-a-rhyme-reactnative/src/screens/ReadingScreen.js
@@ -1,11 +1,44 @@
 import React from "react";
-import {Text, View} from "react-native";
+import {Text, View, Button, TouchableHighlight} from "react-native";
 import {styles} from "../Styles";
+
 
 export const ReadingScreen = () => {
     return(
-        <View style={styles.container}>
-            <Text>This is the reading selection screen</Text>
+
+    <View style={styles.container}>
+        <View style={styles.row}>
+            <TouchableHighlight onPress={this.alert(null)} underlayColor="lightblue">
+                <View style={styles.reading}>
+                    <Text style={styles.readingText}>Reading 1</Text>
+                </View>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={null} underlayColor="lightblue">
+                <View style={styles.reading}>
+                    <Text style={styles.readingText}>Reading 2</Text>
+                </View>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={null} underlayColor="lightblue">
+                <View style={styles.reading}>
+                    <Text style={styles.readingText}>Reading 3</Text>
+                </View>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={null} underlayColor="lightblue">
+                <View style={styles.reading}>
+                    <Text style={styles.readingText}>Reading 4</Text>
+                </View>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={null} underlayColor="lightblue">
+                <View style={styles.reading}>
+                    <Text style={styles.readingText}>Reading 5</Text>
+                </View>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={null} underlayColor="lightblue">
+                <View style={styles.reading}>
+                    <Text style={styles.readingText}>+</Text>
+                </View>
+            </TouchableHighlight>
+            </View>
         </View>
     );
 }
