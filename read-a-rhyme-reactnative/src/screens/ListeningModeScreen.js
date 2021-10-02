c7d32ab65b0c5326ee14f9f59248100f424f24e9
Max <malhasso3@gatech.edu>
2021-10-01 21:24:06 -0400

    Added functionality for generating touchable words

--- read-a-rhyme-reactnative/src/screens/ListeningModeScreen.js
+++ read-a-rhyme-reactnative/src/screens/ListeningModeScreen.js
@@ -5,6 +5,34 @@ import { bookData } from '../bookData';
 export const ListeningModeScreen = (props) => {
     console.log(props.route.params.book);
     const book = props.route.params.book;
+    const readingType = props.route.params.readingType;
+
+    const audioWords = ["mary", "had"];
+
+    function punctuation(str) {
+        if (str != undefined) {
+            if (str.search(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g) != -1) {
+                return("");
+            } else {
+                return(" ");
+            }
+        }
+    }
+
+    function generateTouchableWords() {
+        let words = book.text.match(/\b(\w+)\b|[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g);
+        let x = [];
+        for (let i = 0; i < words.length; i++) {
+            if (audioWords.includes(words[i].toLowerCase())) {
+                x.push(<Text key={"Key_" + i} style={{color:'red'}}>{words[i]}{punctuation(words[i + 1])}</Text>);
+            } else {
+                x.push(<Text key={"Key_" + i}>{words[i]}{punctuation(words[i + 1])}</Text> )
+            }
+        }
+
+        return(<Text>{x}</Text>);
+    }
+
     return (
         <View>
             <Text>{book.title}</Text>
@@ -12,7 +40,7 @@ export const ListeningModeScreen = (props) => {
             <Image source={book.image} style={{ width: 300, height: 400 }} />
             </View>
             <View>
-                <Text>{book.text}</Text>
+                {readingType ? generateTouchableWords() : <Text>{book.text}</Text>}
             </View>
         </View>
     )
